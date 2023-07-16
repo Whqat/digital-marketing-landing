44a81563845cff90c8376211d20952185ee18c27
Whqat <128741039+Whqat@users.noreply.github.com>
2023-07-16 17:18:17 +0300

    re-add popper

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['@popperjs/core']
-    }
-  }
 })
