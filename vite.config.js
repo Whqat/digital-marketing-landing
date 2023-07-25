5b8af089ebef8beeb5aec9f64c1221b94e2a999c
Whqat <128741039+Whqat@users.noreply.github.com>
2023-07-25 16:32:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-
   plugins: [
-    vue({
+    [vue({
       template: {
         compilerOptions: {
           delimiters: ['[[', ']]']
         }
       }
-    })
+    })],
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
 })
