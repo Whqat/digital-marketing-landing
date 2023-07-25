b6bdde67c8d4700021734c19814aaf619638c8fc
Whqat <128741039+Whqat@users.noreply.github.com>
2023-07-25 15:01:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,23 @@ export default defineConfig({
       }
     })],
   ],
+server: {
+    middleware: [    
+       function (req, res, next) {
+         const type = {
+           '.html': 'text/html',
+           '.js': 'text/javascript',  
+           '.css': 'text/css'
+         }[path.extname(req.path)];
+         
+         if (type) {
+           res.setHeader('Content-Type', type);
+         }
+         
+         next();
+       }
+    ]    
+  }
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
