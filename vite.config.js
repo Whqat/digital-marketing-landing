552b376f894e94ebe308081f4015c6fb65611b25
Whqat <128741039+Whqat@users.noreply.github.com>
2023-07-25 16:24:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,8 +33,8 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))  
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 
 })
